e39f7b3cae53b9c499a61d55cabf9fa73ce59049
jonas <jonas.gastecker@sz-ybbs.ac.at>
2020-01-02 23:11:56 +0100

    understood that imagesource is important

--- Outline/ImageLogic/ImageProcessor.cs
+++ Outline/ImageLogic/ImageProcessor.cs
@@ -1,32 +1,34 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
 using System.Windows.Media.Imaging;
 
 namespace Outline.ImageLogic
 {
-    public class ImageProcessor
-    {
-        private BitmapImage _image = new BitmapImage();
-        public BitmapImage Image { get; set; }
-
-        public ImageProcessor(BitmapImage image)
-        {
-            Image = image;
-            
-        }
-
-        public WriteableBitmap ProcessWithFilter(Object filter)
-        {
-            int width = (int)Image.Width;
-            int height = (int)Image.Height;
-            Array a = Array.Empty<int>();
-            Image.CopyPixels(a, 4, 0);
-            WriteableBitmap bitmapImage = new WriteableBitmap(width, height, (int)Image.DpiX, (int)Image.DpiY, Image.Format, Image.Palette);
-            bitmapImage.WritePixels(new Int32Rect(0, 0, width, height), a, 0, 0);
-            BitmapImage image = new BitmapImage();
-            return bitmapImage;
-        }
-        
-        
-    }
+//    public class ImageProcessor
+//    {
+//        
+//        private BitmapImage _image = new BitmapImage();
+////        public BitmapImage Image { get; set; }
+//
+//        public ImageProcessor(BitmapImage image)
+//        {
+//            Bitmap b = new Bitmap();
+//        }
+//
+////        public WriteableBitmap ProcessWithFilter(Object filter)
+////        {
+////            int width = (int)Image.Width;
+////            int height = (int)Image.Height;
+////            Array a = Array.Empty<int>();
+////            Image.CopyPixels(a, 4, 0);
+////            WriteableBitmap bitmapImage = new WriteableBitmap(width, height, (int)Image.DpiX, (int)Image.DpiY, Image.Format, Image.Palette);
+////            bitmapImage.WritePixels(new Int32Rect(0, 0, width, height), a, 0, 0);
+////            BitmapImage image = new BitmapImage();
+////            return bitmapImage;
+////        }
+//        
+//        
+//    }
 }
--- Outline/ViewModel/ImageManager.cs
+++ Outline/ViewModel/ImageManager.cs
@@ -1,24 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using Microsoft.Win32;
 using Outline.Annotations;
-using Outline.ImageLogic;
 
 namespace Outline.ViewModel
 {
     public class ImageManager : INotifyPropertyChanged
     {
-        private BitmapImage _openedImage;
-        private WriteableBitmap _resultingImage;
+        private BitmapSource _openedImage;
+        private BitmapSource _resultingImage;
 
-        public BitmapImage OpenedImage{
+        public BitmapSource OpenedImage{
             get => _openedImage;
             set
             {
@@ -26,11 +26,17 @@ namespace Outline.ViewModel
                 OnPropertyChanged(nameof(OpenedImage));
             }
         }
-
-        public WriteableBitmap ResultingImage{
+        
+        public BitmapSource ResultingImage{
             get => _resultingImage;
             set
             {
+//                if (value.GetType() == typeof(Image))
+//                {
+//                    
+//                    Console.WriteLine(value.Source);
+//                    Console.WriteLine(value.Source);
+//                }
                 _resultingImage = value;
                 OnPropertyChanged(nameof(ResultingImage));
             }
@@ -40,11 +46,39 @@ namespace Outline.ViewModel
 
         public ImageManager()
         {
+//            Stream imageStreamSource = new FileStream("fane.jpg", FileMode.Open, FileAccess.Read, FileShare.Read);
+//            JpegBitmapDecoder decoder = new JpegBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+//            OpenedImage = decoder.Frames[0];
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("fane.jpg", UriKind.Relative);
+            image.EndInit();
+            OpenedImage = image;
+
+            Console.WriteLine(GetPixel(OpenedImage));
+            
             OpenImage = new RelayCommand(o =>
             {
                 OpenedImage = OpenImageFromFileSystem();
-                var im = new ImageProcessor(OpenedImage);
-                ResultingImage = im.ProcessWithFilter(new object());
+
+                FormatConvertedBitmap newFormatedBitmapSource = new FormatConvertedBitmap();
+                newFormatedBitmapSource.BeginInit();
+                newFormatedBitmapSource.Source = OpenedImage;
+
+                List<System.Windows.Media.Color> colors = new List<System.Windows.Media.Color>();
+                colors.Add(System.Windows.Media.Colors.Red);
+                colors.Add(System.Windows.Media.Colors.Blue);
+                BitmapPalette myPalette = new BitmapPalette(colors);
+
+                // Set the DestinationPalette property to the custom palette created above.
+                newFormatedBitmapSource.DestinationPalette = myPalette;
+
+                // Set the DestinationFormat to the palletized pixel format of Indexed1.
+                newFormatedBitmapSource.DestinationFormat = PixelFormats.Indexed1;
+                newFormatedBitmapSource.EndInit();
+                
+                ResultingImage = newFormatedBitmapSource;
             });
             OpenImage.CanExecute(true);
         }
@@ -52,10 +86,53 @@ namespace Outline.ViewModel
         private BitmapImage OpenImageFromFileSystem()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            BitmapImage image = new BitmapImage();
             if(openFileDialog.ShowDialog() == true)
-//                return File.ReadAllText(openFileDialog.FileName);
-                return new BitmapImage(new Uri(openFileDialog.FileName));
-            return new BitmapImage();
+            {
+                image = new BitmapImage(new Uri(openFileDialog.FileName));
+//                image.DecodePixelWidth = 200;
+            }
+            return image;
+        }
+
+        private BitmapSource CreateFromPixel(int width, int height, byte[] pixels)
+        {
+            BitmapPalette myPalette = BitmapPalettes.Halftone256;
+
+            BitmapSource image = BitmapSource.Create(
+                width,
+                height,
+                96,
+                96,
+                PixelFormats.Indexed8,
+                myPalette,
+                pixels,
+                width);
+            return image;
+        }
+
+        private byte[] GetPixel(BitmapSource image)
+        {
+            int width = image.PixelWidth;
+            int height = image.PixelHeight;
+            int stride = (image.PixelWidth * image.Format.BitsPerPixel + 7) / 8;
+            byte[] pixels = new byte[width * stride];
+            Console.WriteLine(stride);
+            image.CopyPixels(pixels, stride, 0);
+
+            int i = 0;
+            var query = from s in pixels
+                let num = i++
+                group s by num / 4 into g
+                select g.ToArray();
+
+            byte[][] groupedPixel = query.ToArray();
+            
+            foreach (var pixel in groupedPixel)
+            {
+                Console.WriteLine($"alpha: {pixel[0],2}, red: {pixel[1],2}, green: {pixel[2],2}, blue: {pixel[3],2}");
+            }
+            return pixels;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
