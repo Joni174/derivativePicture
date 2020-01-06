712438b78af476efdf708eceb8e1f77e4e8defc1
Jonas Gastecker <jonas.gastecker@sz-ybbs.ac.at>
2020-01-06 23:02:49 +0100

    Serialize and deserialize images into pixel arrays

--- Outline/ImageLogic/ImageProcessor.cs
+++ Outline/ImageLogic/ImageProcessor.cs
@@ -2,33 +2,117 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Outline.ImageLogic
 {
-//    public class ImageProcessor
-//    {
-//        
-//        private BitmapImage _image = new BitmapImage();
-////        public BitmapImage Image { get; set; }
-//
-//        public ImageProcessor(BitmapImage image)
-//        {
-//            Bitmap b = new Bitmap();
-//        }
-//
-////        public WriteableBitmap ProcessWithFilter(Object filter)
-////        {
-////            int width = (int)Image.Width;
-////            int height = (int)Image.Height;
-////            Array a = Array.Empty<int>();
-////            Image.CopyPixels(a, 4, 0);
-////            WriteableBitmap bitmapImage = new WriteableBitmap(width, height, (int)Image.DpiX, (int)Image.DpiY, Image.Format, Image.Palette);
-////            bitmapImage.WritePixels(new Int32Rect(0, 0, width, height), a, 0, 0);
-////            BitmapImage image = new BitmapImage();
-////            return bitmapImage;
-////        }
-//        
-//        
-//    }
+    class ImageProcessor
+    {
+        public BitmapSource Image { get; set; }
+
+        public ImageProcessor(BitmapSource image)
+        {
+            Image = image;
+        }
+
+        public BitmapSource ProcessWithFilter()
+        {
+            int width = Image.PixelWidth;
+            var pixels = GetPixel(Image);
+            int i = 0;
+            var query = from s in pixels
+                let num = i++
+                group s by num / 4 into g
+                select g.ToArray();
+            byte[][] groupedPixel = query.ToArray();
+            i = 0;
+            var other = from s in groupedPixel
+                let num = i++
+                group s by num / width into g
+                select g.ToArray();
+            byte[][][] pixelField = other.ToArray();
+            byte[][][] otherField = new byte[pixelField.Length-2][][];
+
+            for (i = 1; i < pixelField.Length-1; i++)
+            {
+                otherField[i-1] = new byte[pixelField[i-1].Length-2][];
+                for (int j = 1; j < pixelField[i].Length-1; j++)
+                {
+                    byte[][] compare = new[]
+                    {
+                        pixelField[i - 1][j - 1],
+                        pixelField[i - 1][j + 1],
+                        pixelField[i + 1][j - 1],
+                        pixelField[i + 1][j + 1],
+                    };
+
+                    int biggestContrast = 0;
+                    for (int k = 0; k < compare.Length; k++)
+                    {
+                        int kr = pixelField[i][j][0] - compare[k][0];
+                        int kb = pixelField[i][j][1] - compare[k][1];
+                        int kg = pixelField[i][j][2] - compare[k][2];
+                        int contrast = (Math.Abs(kr) + Math.Abs(kb) + Math.Abs(kg)) / 3;
+                        if (contrast > biggestContrast) biggestContrast = contrast;
+                    }
+
+                    int greyTone = 255 - biggestContrast;
+//                    otherField[i - 1][j - 1] = new[] {(byte)1, (byte)1, (byte)1, (byte)255};
+                    otherField[i - 1][j - 1] = new[] {(byte)greyTone, (byte)greyTone, (byte)greyTone, (byte)255};
+                }
+            }
+
+            byte[] solution = new byte[otherField.Length * otherField[0].Length * 4];
+            int c = 0;
+            for (int j = 0; j < otherField.Length; j++)
+            {
+                for (int k = 0; k < otherField[j].Length; k++)
+                {
+                    
+                    for (int l = 0; l < otherField[j][k].Length; l++)
+                    {
+                        solution[c] = otherField[j][k][l];
+                        c++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"rows: {pixelField.Length}, columns: {pixelField[0].Length}");
+            return CreateFromPixel(Image.PixelWidth - 2, Image.PixelHeight - 2, solution);
+        }
+        private byte[] GetPixel(BitmapSource image)
+        {
+            int width = image.PixelWidth;
+            int height = image.PixelHeight;
+            int stride = (image.PixelWidth * image.Format.BitsPerPixel + 7) / 8;
+            byte[] pixels = new byte[stride * height];
+            image.CopyPixels(pixels, stride, 0);
+
+            int i = 0;
+            var query = from s in pixels
+                let num = i++
+                group s by num / 4 into g
+                select g.ToArray();
+//            byte[][] groupedPixel = query.ToArray();
+            return pixels;
+        }
+        private BitmapSource CreateFromPixel(int width, int height, byte[] pixels)
+        {
+            Console.WriteLine(PixelFormats.Bgr32.BitsPerPixel);
+            BitmapPalette myPalette = BitmapPalettes.Halftone256;
+            BitmapSource image = BitmapSource.Create(
+                width,
+                height,
+                96,
+                96,
+                PixelFormats.Bgr32,
+                myPalette,
+                pixels,
+                width*4);
+            return image;
+        }
+    }
 }
--- Outline/ViewModel/ImageManager.cs
+++ Outline/ViewModel/ImageManager.cs
@@ -10,7 +10,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
+using Microsoft.Win32.SafeHandles;
 using Outline.Annotations;
+using Outline.ImageLogic;
 
 
 //TODO: Reordering of program structure
@@ -35,60 +37,47 @@ namespace Outline.ViewModel
             get => _resultingImage;
             set
             {
-//                if (value.GetType() == typeof(Image))
-//                {
-//                    
-//                    Console.WriteLine(value.Source);
-//                    Console.WriteLine(value.Source);
-//                }
                 _resultingImage = value;
                 OnPropertyChanged(nameof(ResultingImage));
             }
         }
-
         public ICommand OpenImage { get; private set; }
-
+        public ICommand ExportImage { get; private set; }
         public ImageManager()
         {
-//            Stream imageStreamSource = new FileStream("fane.jpg", FileMode.Open, FileAccess.Read, FileShare.Read);
-//            JpegBitmapDecoder decoder = new JpegBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-//            OpenedImage = decoder.Frames[0];
-
             BitmapImage image = new BitmapImage();
             image.BeginInit();
             image.UriSource = new Uri(@"C:\Users\Jonas\Pictures\DoctorWhoWallpaper.jpg");
-            image.DecodePixelWidth = 50;
+            image.DecodePixelWidth = 400;
             image.EndInit();
             OpenedImage = image;
 
-            
-            var pixels = GetPixel(OpenedImage);
-            
             OpenImage = new RelayCommand(o =>
             {
-                OpenedImage = CreateFromPixel(image.PixelWidth, image.PixelHeight, pixels);
-                
-                /*OpenedImage = OpenImageFromFileSystem();
-
-                FormatConvertedBitmap newFormatedBitmapSource = new FormatConvertedBitmap();
-                newFormatedBitmapSource.BeginInit();
-                newFormatedBitmapSource.Source = OpenedImage;
-
-                List<System.Windows.Media.Color> colors = new List<System.Windows.Media.Color>();
-                colors.Add(System.Windows.Media.Colors.Red);
-                colors.Add(System.Windows.Media.Colors.Blue);
-                BitmapPalette myPalette = new BitmapPalette(colors);
-
-                // Set the DestinationPalette property to the custom palette created above.
-                newFormatedBitmapSource.DestinationPalette = myPalette;
-
-                // Set the DestinationFormat to the palletized pixel format of Indexed1.
-                newFormatedBitmapSource.DestinationFormat = PixelFormats.Indexed1;
-                newFormatedBitmapSource.EndInit();
-                
-                ResultingImage = newFormatedBitmapSource;*/
+                OpenedImage = OpenImageFromFileSystem();
+                ImageProcessor imageProcessor = new ImageProcessor(OpenedImage);
+                ResultingImage = imageProcessor.ProcessWithFilter();
+            });
+            
+            ExportImage = new RelayCommand(o =>
+            {
+                WriteImageToFileSystem(ResultingImage);
             });
             OpenImage.CanExecute(true);
+        }
+
+        private void WriteImageToFileSystem(BitmapSource resultingImage)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                BitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(resultingImage));
+                using (FileStream file = File.OpenWrite(saveFileDialog.FileName))
+                {
+                    encoder.Save(file);
+                }
+            }
         }
 
         private BitmapImage OpenImageFromFileSystem()
@@ -101,43 +90,8 @@ namespace Outline.ViewModel
             }
             return image;
         }
-
-        private BitmapSource CreateFromPixel(int width, int height, byte[] pixels)
-        {
-            Console.WriteLine(PixelFormats.Bgr32.BitsPerPixel);
-            BitmapPalette myPalette = BitmapPalettes.Halftone256;
-            BitmapSource image = BitmapSource.Create(
-                width,
-                height,
-                96,
-                96,
-                PixelFormats.Bgr32,
-                myPalette,
-                pixels,
-                width*4);
-            return image;
-        }
-
-        private byte[] GetPixel(BitmapSource image)
-        {
-            int width = image.PixelWidth;
-            int height = image.PixelHeight;
-            int stride = (image.PixelWidth * image.Format.BitsPerPixel + 7) / 8;
-            byte[] pixels = new byte[stride * height];
-            image.CopyPixels(pixels, stride, 0);
-
-            int i = 0;
-            var query = from s in pixels
-                let num = i++
-                group s by num / 4 into g
-                select g.ToArray();
-
-//            byte[][] groupedPixel = query.ToArray();
-            return pixels;
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
-
+        
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
